dd7c391fa6c1cad9d926817eb4e6e79e11992597
missirvlad <147083170+missirvlad@users.noreply.github.com>
2024-03-17 14:35:04 +0500

    upload files

---
+++ models/Reservation.java
@@ -0,0 +1,30 @@
+package models;
+
+import java.util.Date;
+
+
+public class Reservation {
+    private static int counter = 1000;
+    private final int id;
+    private Table table;
+    private Date date;
+    private String nameClients;
+    {
+        id = ++counter;
+    }
+
+    public Reservation(Table table, Date date, String nameClients) {
+        this.table = table;
+        this.date = date;
+        this.nameClients = nameClients;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+}
---
+++ models/Table.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Table {
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Collection<Reservation> getReservations() {
+        return reservations;
+    }
+
+    private final Collection<Reservation> reservations = new ArrayList<>();
+    private static int counter;
+    private final int number;
+    {
+        number = ++counter;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Столик № %d", number);
+    }
+
+}
